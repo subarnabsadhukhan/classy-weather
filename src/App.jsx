20a30188fd32c835bccc5fc5b1fa4f78967e46b6
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-02-05 09:32:58 +0530

    Child to Parent Communication

--- src/App.jsx
+++ src/App.jsx
@@ -59,21 +59,19 @@ class App extends React.Component {
       this.setState({ isLoading: false });
     }
   };
+
+  setLocation = (e) => {
+    return this.setState({ location: e.target.value });
+  };
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
 
-        <div>
-          <input
-            type="text"
-            placeholder="Search for a city..."
-            value={this.state.location}
-            onChange={(e) => {
-              return this.setState({ location: e.target.value });
-            }}
-          />
-        </div>
+        <Input
+          location={this.state.location}
+          onLocationChange={this.setLocation}
+        />
         <button onClick={this.fetchWeather}>Get Weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
         {this.state.weather.weathercode && (
@@ -89,6 +87,20 @@ class App extends React.Component {
 
 export default App;
 
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search for a city..."
+          value={this.props.location}
+          onChange={this.props.onLocationChange}
+        />
+      </div>
+    );
+  }
+}
 class Weather extends React.Component {
   render() {
     console.log(this.props);
